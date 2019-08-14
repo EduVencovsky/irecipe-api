f0bd853986abcf5e65fe6b8aad6bee32b6c27b93
Eduardo Vencovsky <eduvencovsky@gmail.com>
2019-08-14 20:51:15 -0300

    Generate DB imporved

---
+++ db/generatedb.js
@@ -0,0 +1,44 @@
+const axios = require('axios')
+const users = require('./users.json')
+const ingredients = require('./ingredients.json')
+const measurements = require('./measurements.json')
+const uri = 'http://localhost:3000/'
+
+const consoleThen = res => console.log(res.data)
+const consoleCatch = error => console.log(error.response.data)
+
+let token = ''
+
+axios.default.defaults.baseURL = uri
+
+const generatedb = async () => {
+    const usersPromises = users.map(user => axios.post('/api/user', user))
+    await Promise.all(usersPromises)
+        .then(values => consoleThen(values))
+        .catch(consoleCatch)
+
+    await axios
+        .post('/auth/signin', users[0])
+        .then(res => {
+            token = res.data.token
+            console.log('Current User ' + users[0])
+            console.log('Token ' + token)
+        })
+        .catch(consoleCatch)
+
+    axios.default.defaults.headers.common.Authorization = 'Bearer ' + token
+
+    const ingredientsPromises = ingredients.map(ingredient =>
+        axios.post('/api/ingredient', ingredient)
+    )
+    await Promise.all(ingredientsPromises)
+        .then(values => consoleThen(values))
+        .catch(consoleCatch)
+
+    const measurementsPromises = measurements.map(measurement =>
+        axios.post('/api/measurement', measurement)
+    )
+    await Promise.all(measurementsPromises)
+        .then(values => consoleThen(values))
+        .catch(consoleCatch)
+}
