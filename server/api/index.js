7135d2ec137c0b723e2864b7d6407955d5d0d5b8
Eduardo Vencovsky <eduvencovsky@gmail.com>
2019-10-30 22:15:24 -0300

    Routing Fix

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,14 @@
 const router = require('express').Router()
-const user = require('./user/userRoutes')
-const measurement = require('./measurement/measurementRoutes')
-const recipe = require('./recipe/recipeRoutes')
-const ingredient = require('./ingredient/ingredientRoutes')
 
-// router.use('/', (req, res, next) => res.send('Hello World!'))
+const user = require('./user/userRoutes')
+const recipe = require('./recipe/recipeRoutes')
+const appliance = require('./appliance/applianceRoutes')
+const ingredient = require('./ingredient/ingredientRoutes')
+const measurement = require('./measurement/measurementRoutes')
+
 router.use('/user', user)
 router.use('/recipe', recipe)
+router.use('/appliance', appliance)
 router.use('/ingredient', ingredient)
 router.use('/measurement', measurement)
 
